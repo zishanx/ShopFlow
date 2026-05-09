183fee1ac0999e54f99e6af66a48be245d11ec11
zishanx <zishanalikhan53@gmail.com>
2026-05-10 00:12:15 +0530

    saving

---
+++ Backend/middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+import jwt, { decode } from 'jsonwebtoken'
+
+const protect = async (req, res, next) => {
+
+    try {
+        const token = req.headers.authorization?.split(" ")[1];
+        if (!token) {
+            return res.status(401).json({ message: "No Token Available." })
+        }
+
+        const decoded =  jwt.verify(token,process.env.JWT_SECRET,)
+
+        req.user = decoded
+
+        next()
+
+    } catch (error) {
+res.status(400).json({error: error.message})
+    }
+}
+
+export default protect
