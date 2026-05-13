38ca4a8941243aeff03f42a4181c47dc96f601cd
zishanx <zishanalikhan53@gmail.com>
2026-05-13 09:45:29 +0530

    Done with the hero sectoin

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,15 +1,30 @@
-export default function Home(){
+export default function Home() {
     return (
         <>
-            <div className="hero p-30 grid grid-cols-2">
-                <div className="disc">
-                <p>New arrivals every week</p>
-                <h1>Shop <br />Smarter, <br />Live <br />Better.</h1>
-                <p>Discover curated products across every category. From everyday essentials to premium finds -- all in one place</p>
+            <div className="hero bg-[#ffffff] p-30 grid grid-cols-2  ">
+                <div className="disc flex
+                flex-col gap-7">
+                    <p className="p-2 bg-[#fff0f2] text-[#FF3D5A] rounded-xl w-fit">New arrivals every week</p>
+                    <h1 className="font-extrabold font-[Syne] text-6xl ">Shop <br />Smarter, <br />Live <br /><span className="text-[#FF3D5A]">Better.</span></h1>
+                    <p className="text-[#666666] w-2/3">Discover curated products across every category. From everyday essentials to premium finds -- all in one place</p>
+                    <div className="buttons flex gap-3">
+                        <button className="bg-[#FF3D5A] text-white p-3 rounded-xl hover:bg-[#e0002a]">Shop now</button>
+                        <button className="bg-[#fff0f2] border border-[#e0e0e0]  text-[#0a0a0a] p-3 rounded-xl">Browse Categories</button>
+                    </div>
                 </div>
                 <div className="hero-img flex gap-5 items-center justify-center">
-                    <img src="https://picsum.photos/id/26/300/300" alt="" />
-                    <img src="https://picsum.photos/id/27/300/300" alt="" />
+                    <div className="card p-4 bg-[#f9f9f9] rounded-md">
+
+                        <img src="https://picsum.photos/id/26/300/300" alt="" />
+                        <p>Air Runners</p>
+                        <p className="text-[#FF3D5A] font-bold">$129</p>
+                    </div>
+                    <div className="card p-4 bg-[#f9f9f9] relative top-10 rounded-md">
+
+                        <img src="https://picsum.photos/id/27/300/300" alt="" />
+                        <p>Pro Headset</p>
+                        <p className="text-[#FF3D5A] font-bold  rounded-md">$249</p>
+                    </div>
 
                 </div>
             </div>
