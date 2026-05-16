cfcf459a0c71ec2c8ec51e6621190fc9f6db8370
zishanx <zishanalikhan53@gmail.com>
2026-05-16 12:46:16 +0530

    Worked on the Cart model

--- Backend/models/Cart.js
+++ Backend/models/Cart.js
@@ -1,1 +1,18 @@
 import mongoose from 'mongoose'
+
+const cartSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    items: [
+        {
+            product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+            name: { type: String, required: true },
+            image: { type: String, required: true },
+            price: { type: Number, required: true },
+            quantity: { type: Number, required: true },
+        }
+    ]
+})
+
+const Cart = mongoose.model('Cart', cartSchema)
+
+export default Cart
