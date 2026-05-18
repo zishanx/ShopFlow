e1e43add7d543199d5521ca914bf078b883c1eec
zishanx <zishanalikhan53@gmail.com>
2026-05-18 11:25:17 +0530

    Deployed the backend

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: `http://localhost:5000/api`
+    baseURL: `https://shopflow-production-89d1.up.railway.app`
 })
 
 api.interceptors.request.use((config)=>{
